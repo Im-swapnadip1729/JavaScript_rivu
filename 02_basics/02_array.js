cf76c9b5531379c0f9d6e93fdf0b6b003af0db90
swapnadip98 <123538663+swapnadip98@users.noreply.github.com>
2023-11-29 22:09:41 +0530

    PACKED vs HOLEY

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -27,3 +27,12 @@ let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+
+// Summary:
+// There are two types of arrays: holey arrays and continuous arrays. These arrays can contain three types of elements: SMI (Small Integer), Packed Element, and Double.
+
+// 1. **Holey Array**: A holey array is an array that contains missing or empty elements. For example, [1, 2, , 4] is a holey array because it has a missing element. Holey arrays are less optimized for performance.
+
+// 2. **Continuous Array**: A continuous array is an array that does not contain missing or empty elements. For example, [1, 2, 3, 4] is a continuous array because it has no missing elements. Continuous arrays are more optimized for performance.
+
+// In terms of optimization, continuous arrays are more efficient and optimized for various operations compared to holey arrays.
